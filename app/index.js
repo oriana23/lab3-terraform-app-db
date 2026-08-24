9c6151927900f94a38175d3a542254b8d3985d66
GonGarciaFontenla <ggarciafontenla@frba.utn.edu.ar>
2026-08-24 17:14:17 -0300

    probando pipeline

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { initDB } = require('./src/config/db');
 const notasRouter = require('./src/routes/notas');
- 
+  
 const app = express();
 const PORT = process.env.PORT || 3000;
 
